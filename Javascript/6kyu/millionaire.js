f534c8167aee5edd3dc2971fac042d1a249a90dc
kevinmulier <48089250+kevinmulier@users.noreply.github.com>
2023-01-11 09:14:47 +0100

    -

--- Javascript/6kyu/millionaire.js
+++ Javascript/6kyu/millionaire.js
@@ -3,7 +3,6 @@
 // www.codewars.com/kata/6384c9ec84c67000230a15f1/train/javascript
 
 // My solution
-
 function getTotalCashPrize(prizeFund, correctAnswers, playerActions) {
   let result = 0;
   let lifelinesUsed = 0;
@@ -12,17 +11,33 @@ function getTotalCashPrize(prizeFund, correctAnswers, playerActions) {
     if (i === 5 || i === 11) {
       safeHaven = result;
     }
-    if (playerActions[i].split("").includes("1") || playerActions[i].split("").includes("2") || playerActions[i].split("").includes("3")) {
-      lifelinesUsed += playerActions[i].split("").length - 1;
-      if (playerActions[i].split("").includes("X")) {
-        console.log(`Used lifeline at question ${i + 1}. Total lifelines used : ${lifelinesUsed}`);
-        console.log(`Left at question ${i + 1} after host asked question`);
-        return [safeHaven, lifelinesUsed];
-      } else if (playerActions[i].split("").includes("W")) {
-        console.log(`Used lifeline at question ${i + 1}. Total lifelines used : ${lifelinesUsed}`);
-        console.log(`Left at question ${i + 1} before host asked question`);
-        return [result, lifelinesUsed];
+    
+//     Lifelines cases
+   if (playerActions[i].split("").includes("1") || playerActions[i].split("").includes("2") || playerActions[i].split("").includes("3")) {
+     lifelinesUsed += playerActions[i].split("").length - 1;
+     if (playerActions[i].split("").includes("X")) {
+       console.log(`Used lifeline at question ${i+1}. Total lifelines used : ${lifelinesUsed}`)
+          console.log(`Left at question ${i+1} after host asked question`)
+        }
+     else if (playerActions[i].split("").includes("W")) {
+       console.log(`Used lifeline at question ${i+1}. Total lifelines used : ${lifelinesUsed}`)
+       console.log(`Left at question ${i+1} before host asked question`)
+     }
+     else if (playerActions[i].split("").includes(correctAnswers[i])) {
+       result += prizeFund[i];
+       console.log(`Used lifeline at question ${i+1}. Total lifelines used : ${lifelinesUsed}`)
+       console.log(`Correct answer at question ${i+1}. Current prize = ${result}`)
+     }
+     else {
+       console.log(`Used lifeline at question ${i+1}. Total lifelines used : ${lifelinesUsed}`)
+       console.log(`Wrong answer at question ${i+1}. Final prize = 0`)
+     }
       }
+//     Correct answer 
+    else if (playerActions[i] === correctAnswers[i]) {
+      result += prizeFund[i]
     }
-  }
+    else if (playerActions[i] !== correctAnswers[i] || (playerActions[i] !== "W") ||
+    }
+  return [result, lifelinesUsed];
 }
