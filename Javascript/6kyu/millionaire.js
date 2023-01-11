8326207675b4efe571c9a331dc0842501db71c5f
kevinmulier <48089250+kevinmulier@users.noreply.github.com>
2023-01-11 13:50:29 +0100

    update 6kyu - done

--- Javascript/6kyu/millionaire.js
+++ Javascript/6kyu/millionaire.js
@@ -1,8 +1,3 @@
-// Kata
-
-// www.codewars.com/kata/6384c9ec84c67000230a15f1/train/javascript
-
-// My solution
 function getTotalCashPrize(prizeFund, correctAnswers, playerActions) {
   let result = 0;
   let lifelinesUsed = 0;
@@ -16,29 +11,22 @@ function getTotalCashPrize(prizeFund, correctAnswers, playerActions) {
     if (playerActions[i].split("").includes("1") || playerActions[i].split("").includes("2") || playerActions[i].split("").includes("3")) {
       lifelinesUsed += playerActions[i].split("").length - 1;
       if (playerActions[i].split("").includes("X")) {
-        console.log(`Used lifeline at question ${i + 1}. Total lifelines used : ${lifelinesUsed}`);
-        console.log(`Left at question ${i + 1} after host asked question`);
-      } else if (playerActions[i].split("").includes("W")) {
-        console.log(`Used lifeline at question ${i + 1}. Total lifelines used : ${lifelinesUsed}`);
-        console.log(`Left at question ${i + 1} before host asked question`);
+        result = safeHaven;
       } else if (playerActions[i].split("").includes(correctAnswers[i])) {
         result += prizeFund[i];
-        console.log(`Used lifeline at question ${i + 1}. Total lifelines used : ${lifelinesUsed}`);
-        console.log(`Correct answer at question ${i + 1}. Current prize = ${result}`);
-      } else {
-        console.log(`Used lifeline at question ${i + 1}. Total lifelines used : ${lifelinesUsed}`);
-        console.log(`Wrong answer at question ${i + 1}. Final prize = 0`);
+      } else if (playerActions[i].split("").includes(correctAnswers[i]) === false) {
+        result = 0;
       }
     }
-    //     Exit after question
+    //     Exit after question = safehaven
     else if (playerActions[i] === "X") {
       result = safeHaven;
     }
-    //     Correct answer
+    //     Correct answer = +prize to result
     else if (playerActions[i] === correctAnswers[i]) {
       result += prizeFund[i];
     }
-    //     Wrong answer
+    //     Wrong answer = loose everything
     else if (playerActions[i] !== correctAnswers[i] && playerActions[i] !== "W") {
       result = 0;
     }
