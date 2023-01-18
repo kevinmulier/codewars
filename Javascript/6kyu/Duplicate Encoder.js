18af9af466d8cf2f2017fea6a8e728f9dc3604a5
kevinmulier <kevin.mulier@outlook.fr>
2023-01-18 19:39:36 +0100

    6 kyu (learned about lastIndexOf)

---
+++ Javascript/6kyu/Duplicate Encoder.js
@@ -0,0 +1,52 @@
+// Kata
+
+// The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+// Examples
+// "din"      =>  "((("
+// "recede"   =>  "()()()"
+// "Success"  =>  ")())())"
+// "(( @"     =>  "))(("
+// Notes
+// Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
+
+// My Solution
+
+function duplicateEncode(word) {
+  // ...
+  word = word.toLowerCase().split("");
+  let finalString = "";
+  let doublons = [];
+  for (let i = 0; i < word.length; i++) {
+    let arrCompare = word;
+    let currentWord = word[i];
+    arrCompare[i] = 0;
+    console.log(arrCompare);
+    if (arrCompare.includes(currentWord) || doublons.includes(currentWord)) {
+      finalString += ")";
+    } else {
+      finalString += "(";
+    }
+    doublons.push(currentWord);
+  }
+  return finalString;
+}
+
+// Interesting solutions
+// 1 (use lastIndexOf)
+
+function duplicateEncode(word) {
+  return word
+    .toLowerCase()
+    .split("")
+    .map(function (a, i, w) {
+      return w.indexOf(a) == w.lastIndexOf(a) ? "(" : ")";
+    })
+    .join("");
+}
+
+// 2 (also use regex)
+function duplicateEncode(word) {
+  word = word.toLowerCase();
+  return word.replace(/./g, (m) => (word.indexOf(m) == word.lastIndexOf(m) ? "(" : ")"));
+}
