d18972bb717b984a1be1e6156eefcab4b6119c75
kevinmulier <48089250+kevinmulier@users.noreply.github.com>
2023-01-24 18:50:43 +0100

    Refactored

--- Javascript/7kyu/Highest profit.js
+++ Javascript/7kyu/Highest profit.js
@@ -14,4 +14,4 @@
 // All arrays or lists will always have at least one element, so you don't need to check the length. Also, your function will always get an array or a list, you don't have to check for null, undefined or similar.
 
 // Solution
-const minMax = (arr) => (arr = [Math.min(...arr), Math.max(...arr)]);
+const minMax = (arr) => [Math.min(...arr), Math.max(...arr)];
