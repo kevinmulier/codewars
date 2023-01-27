a995eb1f9ebc1e27bd2c1b846d74d9a5392551b8
kevinmulier <kevin.mulier@outlook.fr>
2023-01-27 23:30:02 +0100

    8kyu

---
+++ Javascript/8kyu/third angle of a triangle.js
@@ -0,0 +1,13 @@
+// Kata
+
+// You are given two interior angles (in degrees) of a triangle.
+
+// Write a function to return the 3rd.
+
+// Note: only positive integers will be tested.
+
+// https://en.wikipedia.org/wiki/Triangle
+
+// Solution
+
+const otherAngle = (a, b) => 180 - a - b;
