74a532e926fc74830662b2793abf5721f470f3db
kevinmulier <kevin.mulier@outlook.fr>
2023-02-05 17:18:31 +0100

    6kyu

--- Javascript/6kyu/unique in order.js
+++ Javascript/6kyu/unique in order.js
@@ -12,14 +12,5 @@
 
 var uniqueInOrder = function (iterable) {
   //your code here - remember iterable can be a string or an array
-  if (iterable.length === 0) {
-    return [];
-  }
-  let finalArr = [iterable[0]];
-  for (let i = 1; i < iterable.length; i++) {
-    if (iterable[i] !== iterable[i - 1]) {
-      finalArr.push(iterable[i]);
-    }
-  }
-  return finalArr;
+  return [...iterable].filter((a, i) => a !== iterable[i - 1]);
 };
