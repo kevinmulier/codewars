7d7ec9f434cccc12671af83994d41af5833c6907
kevinmulier <kevin.mulier@outlook.fr>
2023-03-04 22:52:56 +0100

    7kyu

---
+++ Javascript/7kyu/reverse_letter.js
@@ -0,0 +1,20 @@
+// Task
+// Given a string str, reverse it and omit all non-alphabetic characters.
+
+// Example
+// For str = "krishan", the output should be "nahsirk".
+
+// For str = "ultr53o?n", the output should be "nortlu".
+
+// Input/Output
+// [input] string str
+// A string consists of lowercase latin letters, digits and symbols.
+
+// [output] a string
+
+const reverseLetter = (str) =>
+  str
+    .split("")
+    .filter((e) => /[A-Za-z]+/i.test(e))
+    .reverse()
+    .join("");
