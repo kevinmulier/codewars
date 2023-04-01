134ddf55a5a5bf9a9e1b7035a0c87bfed620cfee
kevinmulier <kevin.mulier@outlook.fr>
2023-04-01 15:47:57 +0200

    7kyu

---
+++ Javascript/7kyu/find_the_capitals.js
@@ -0,0 +1,11 @@
+var capitals = function (word) {
+  const indexesCapitals = [];
+  word.split("").forEach((el, i) => (el.toUpperCase() === el ? indexesCapitals.push(i) : false));
+  return indexesCapitals;
+};
+
+// Instructions
+// Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+
+// Example
+// Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );
