1da18dd15b07adcbe674ce04ac71056a7f7595b5
kevinmulier <kevin.mulier@outlook.fr>
2023-05-06 21:56:13 +0200

    8kyu

---
+++ Javascript/8kyu/position_letter.js
@@ -0,0 +1,7 @@
+// When provided with a letter, return its position in the alphabet.
+
+// Input :: "a"
+
+// Ouput :: "Position of alphabet: 1"
+
+const position = (letter) => `Position of alphabet: ${letter.charCodeAt() - "a".charCodeAt() + 1}`;
