e88b385088f8e3f0071e1a215c27dc252dd31882
kevinmulier <kevin.mulier@outlook.fr>
2023-05-12 23:11:23 +0200

    7kyu

--- Javascript/7kyu/largest_pair.js
+++ Javascript/7kyu/largest_pair.js
@@ -5,3 +5,15 @@
 // [10, 14, 2, 23, 19] -->  42 (= 23 + 19)
 // [99, 2, 2, 23, 19]  --> 122 (= 99 + 23)
 // Input sequence contains minimum two elements and every element is an integer.
+
+function largestPairSum(numbers) {
+  let bestPairResult = numbers[0] + numbers[1];
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i; j < numbers.length; j++) {
+      if (i != j && numbers[i] + numbers[j] > bestPairResult) {
+        bestPairResult = numbers[i] + numbers[j];
+      }
+    }
+  }
+  return bestPairResult;
+}
