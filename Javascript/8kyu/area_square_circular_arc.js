ac554bdc56e94d6af315466650e00eafb0ceb64b
kevinmulier <kevin.mulier@outlook.fr>
2023-05-27 20:53:50 +0200

    8kyu

---
+++ Javascript/8kyu/area_square_circular_arc.js
@@ -0,0 +1,10 @@
+function squareArea(A) {
+  let r = (A * 4) / (2 * Math.PI);
+  return r * r;
+}
+
+// Complete the function that calculates the area of the red square, when the length of the circular arc A is given as the input. Return the result rounded to two decimals.
+
+// Graph
+
+// Note: use the π value provided in your language (Math::PI, M_PI, math.pi, etc)
