44325bbb4a52da1f7624209e82a454394a39d08b
kevinmulier <kevin.mulier@outlook.fr>
2023-08-21 20:38:55 +0200

    7kyu

---
+++ Javascript/7kyu/factorial_func.js
@@ -0,0 +1,11 @@
+function factorial(n) {
+  let result = 1;
+  for (let i = 1; i <= n; i++) {
+    result = result * i;
+  }
+  return result;
+}
+
+// Your task is to write function factorial.
+
+// https://en.wikipedia.org/wiki/Factorial
