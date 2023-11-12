42612bdff6949f0290614bcdad7c830bfb6dad7b
kevinmulier <kevin.mulier@outlook.fr>
2023-11-12 23:14:59 +0100

    7kyu

---
+++ Javascript/7kyu/count_frequent.js
@@ -0,0 +1,19 @@
+function mostFrequentItemCount(collection) {
+  let counts = {};
+
+  for (let el of collection) {
+    if (!counts[el]) {
+      counts[el] = 0;
+    }
+    counts[el]++;
+  }
+
+  return Object.values(counts).length > 0 ? Math.max(...Object.values(counts)) : 0;
+}
+
+// Complete the function to find the count of the most frequent item of an array. You can assume that input is an array of integers. For an empty array return 0
+
+// Example
+// input array: [3, -1, -1, -1, 2, 3, -1, 3, -1, 2, 4, 9, 3]
+// ouptut: 5
+// The most frequent number in the array is -1 and it occurs 5 times.
