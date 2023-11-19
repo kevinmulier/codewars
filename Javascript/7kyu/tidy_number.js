e1683afc8b8d2446b74cd107bc79bc963dfbbc80
kevinmulier <kevin.mulier@outlook.fr>
2023-11-19 22:05:12 +0100

    7kyu

---
+++ Javascript/7kyu/tidy_number.js
@@ -0,0 +1,7 @@
+const tidyNumber = (n) => Number(n.toString().split("").sort().join("")) == n;
+
+//   Definition
+// A Tidy number is a number whose digits are in non-decreasing order.
+
+// Task
+// Given a number, Find if it is Tidy or not .
