08d16d25c589be45a04cd7d35319222b4ad5fffe
kevinmulier <kevin.mulier@outlook.fr>
2023-12-09 19:36:10 +0100

    7kyu

---
+++ Javascript/7kyu/n_highest_elements.js
@@ -0,0 +1,12 @@
+const largest = (n, array) =>
+  array
+    .sort((a, b) => b - a)
+    .slice(0, n)
+    .sort((a, b) => a - b);
+
+// Write a program that outputs the top n elements from a list.
+
+// Example:
+
+// largest(2, [7,6,5,4,3,2,1])
+// // => [6,7]
