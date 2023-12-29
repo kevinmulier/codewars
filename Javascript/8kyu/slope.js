ed708f3910f8b13ce8970e454780e64cfa6d56c3
kevinmulier <kevin.mulier@outlook.fr>
2023-12-29 23:18:08 +0100

    8kyu

---
+++ Javascript/8kyu/slope.js
@@ -0,0 +1,23 @@
+// Given an array of 4 integers
+// [a,b,c,d] representing two points (a, b) and (c, d), return a string representation of the slope of the line joining these two points.
+
+// For an undefined slope (division by 0), return undefined . Note that the "undefined" is case-sensitive.
+
+//    a:x1
+//    b:y1
+//    c:x2
+//    d:y2
+// Assume that [a,b,c,d] and the answer are all integers (no floating numbers!). Slope:
+
+function slope(points) {
+  let [x1, y1, x2, y2] = points;
+
+  let deltaX = x2 - x1;
+  let deltaY = y2 - y1;
+
+  if (deltaX === 0) {
+    return "undefined";
+  }
+
+  return String(deltaY / deltaX);
+}
