5bfe72f48c2ea5bbfacf7b329049583a118e479c
kevinmulier <kevin.mulier@outlook.fr>
2024-03-24 22:40:43 +0100

    7kyu

---
+++ Javascript/7kyu/who_s_online.js
@@ -0,0 +1,69 @@
+const whosOnline = (friends) => {
+  const onlineArr = [];
+  const offlineArr = [];
+  const awayArr = [];
+
+  for (let friend of friends) {
+    if (friend.status === 'online') {
+      friend.lastActivity > 10 ? awayArr.push(friend.username) : onlineArr.push(friend.username);
+    } else {
+      offlineArr.push(friend.username);
+    }
+  }
+
+  let returnObject = {};
+
+  if (onlineArr.length > 0) {
+    returnObject.online = onlineArr;
+  }
+
+  if (offlineArr.length > 0) {
+    returnObject.offline = offlineArr;
+  }
+
+  if (awayArr.length > 0) {
+    returnObject.away = awayArr;
+  }
+
+  return returnObject;
+};
+
+// You have a group chat application, but who is online!?
+
+// You want to show your users which of their friends are online and available to chat!
+
+// Given an input of an array of objects containing usernames, status and time since last activity (in mins), create a function to work out who is online, offline and away.
+
+// If someone is online but their lastActivity was more than 10 minutes ago they are to be considered away.
+
+// The input data has the following structure:
+
+// [{
+//   username: 'David',
+//   status: 'online',
+//   lastActivity: 10
+// }, {
+//   username: 'Lucy',
+//   status: 'offline',
+//   lastActivity: 22
+// }, {
+//   username: 'Bob',
+//   status: 'online',
+//   lastActivity: 104
+// }]
+// The corresponding output should look as follows:
+
+// {
+//   online: ['David'],
+//   offline: ['Lucy'],
+//   away: ['Bob']
+// }
+// If for example, no users are online the output should look as follows:
+
+// {
+//   offline: ['Lucy'],
+//   away: ['Bob']
+// }
+// username will always be a string, status will always be either 'online' or 'offline' (UserStatus enum in C#) and lastActivity will always be number >= 0.
+
+// Finally, if you have no friends in your chat application, the input will be an empty array []. In this case you should return an empty object {} (empty Dictionary in C#).
