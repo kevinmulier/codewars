af76f8205827177eafed74e765936f9478e3f398
Kevin Mulier <kevin.mulier@outlook.fr>
2024-04-13 12:31:58 +0200

    7kyu

---
+++ Javascript/7kyu/is_arr_arr.js
@@ -0,0 +1,11 @@
+const arrCheck = (values) => values.every((val) => Array.isArray(val));
+
+// Is every value in the array an array?
+
+// This should only test the second array dimension of the array. The values of the nested arrays don't have to be arrays.
+
+// Examples:
+
+// [[1],[2]] => true
+// ['1','2'] => false
+// [{1:1},{2:2}] => false
