3252ca93d56930f5e53829d9be6404ebbe3c6215
Kevin Mulier <kevin.mulier@outlook.fr>
2024-10-11 09:38:38 +0200

    6kyu

---
+++ Javascript/6kyu/parse_html_colors.js
@@ -0,0 +1,24 @@
+function parseHTMLColor(color) {
+  if (color.startsWith("#")) {
+    if (color.length === 7) {
+      return {
+        r: parseInt(color.slice(1, 3), 16),
+        g: parseInt(color.slice(3, 5), 16),
+        b: parseInt(color.slice(5, 7), 16),
+      };
+    } else if (color.length === 4) {
+      return {
+        r: parseInt(color[1] + color[1], 16),
+        g: parseInt(color[2] + color[2], 16),
+        b: parseInt(color[3] + color[3], 16),
+      };
+    }
+  } else {
+    const hex = PRESET_COLORS[color.toLowerCase()];
+    return {
+      r: parseInt(hex.slice(1, 3), 16),
+      g: parseInt(hex.slice(3, 5), 16),
+      b: parseInt(hex.slice(5, 7), 16),
+    };
+  }
+}
