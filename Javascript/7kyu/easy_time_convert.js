82db4799b07141e3a3df14ff534b3069946dd98d
kevinmulier <kevin.mulier@outlook.fr>
2024-11-09 22:34:48 +0100

    7kyu

---
+++ Javascript/7kyu/easy_time_convert.js
@@ -0,0 +1,9 @@
+const timeConvert = (num) => {
+  if (num <= 0) {
+    return '00:00';
+  }
+
+  const hours = Math.floor(num / 60);
+  const minutes = num % 60;
+  return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
+};
