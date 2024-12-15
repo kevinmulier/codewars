a6dd1a018e43c1ef53159f3fc2202705dbffa88c
kevinmulier <kevin.mulier@outlook.fr>
2024-12-15 18:41:14 +0100

    7kyu

---
+++ Javascript/7kyu/pass_valid.js
@@ -0,0 +1,7 @@
+function password(str) {
+  if (str.length < 8) return false;
+  if (!/[A-Z]/.test(str)) return false;
+  if (!/[a-z]/.test(str)) return false;
+  if (!/[0-9]/.test(str)) return false;
+  return true;
+}
